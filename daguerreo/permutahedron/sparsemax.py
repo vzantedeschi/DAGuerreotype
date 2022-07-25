4d6f994e30cea48a3d771cb1236fee7a185f94bb
Vlad Niculae <vlad@vene.ro>
2022-07-25 15:05:49 +0200

    fast forward

--- daguerreo/permutahedron/sparsemax.py
+++ daguerreo/permutahedron/sparsemax.py
@@ -4,22 +4,38 @@ import numpy as np
 from itertools import permutations
 
 import torch
+from entmax import sparsemax
+
 from ._kbest import KBestRankings
 from .sparsemap import sparsemap_rank
 
 
-def sparsemax_rank(x, max_k=100):
-    x_ = x.cpu()
-    ranker = KBestRankings(x_, max_k)
-    rankings, scores = ranker.compute(max_k)
+def dot_perm(x, pi):
+    """compute <x, pi>
 
-    scores_ = rankings.to(dtype=x_.dtype) @ x_
+    x: torch Tensor, shape [d]
+    pi: torch Tensor[long], shape [k, d]
+    """
 
-    print(rankings)
-    print(scores)
-    print(scores_)
+    # rho = torch.arange(x.shape[0]).to(dtype=x.dtype, device=x.device) + 1
+    # return x[pi] @ rho
+    pi_ = pi.to(dtype=x.dtype) + 1
+    return pi_ @ x
 
-    return
+
+def sparsemax_rank(x, max_k=100, prune_output=True):
+    ranker = KBestRankings(x.to(device='cpu', dtype=torch.double), max_k)
+    rankings, _ = ranker.compute(max_k)
+
+    # recompute scores on clean autodiffable path from x
+    scores = dot_perm(x, rankings)
+    probas = sparsemax(scores)
+
+    if prune_output:
+        mask = probas > 0
+        return probas[mask], rankings[mask]
+    else:
+        return probas, rankings
 
 
 def main():
@@ -29,6 +45,7 @@ def main():
 
     print("sparsemap")
     print(sparsemap_rank(x, max_iter=100, init=False))
+
 
 if __name__ == '__main__':
     main()
