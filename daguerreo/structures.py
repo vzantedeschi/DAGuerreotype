611b024afc8147118a670a4b3dc693459113f25c
franuluc <franuluc@amazon.de>
2022-08-05 11:54:52 +0200

    added eval mode for structures (MAP)

--- daguerreo/structures.py
+++ daguerreo/structures.py
@@ -14,6 +14,7 @@ class Structure(torch.nn.Module):
     def forward(self):
         """
         This is for the unconditional computation of a probability distribution over complete DAG structures.
+        When eval mode, returns the MAP (still as a triplet, with probability 1.)
 
         Returns:
             Triplet => (probabilities, complete_dags, regularization_term)
@@ -33,6 +34,11 @@ class _ScoreVectorStructure(Structure, ABC):
 
     @classmethod
     def initialize(cls, X, args, bilevel=False):
+        """
+        Takes care of dimensionalities, initialization of the structure parameter theta
+         (e.g. using nodes variances), and of any additional hyperparameters from args. Bilevel here is ignored as
+         these objects have the same behaviour both in joint and bilevel optimization settings.
+         """
         d = X.shape[1]  # number of features= number of nodes
 
         # initialize the parameter score vector
@@ -52,14 +58,48 @@ class _ScoreVectorStructure(Structure, ABC):
     def regularizer(self):
         return self.l2_reg_strength * (self.theta**2).sum()
 
+    def forward(self):
+        if self.training:
+            return self._training_forward()
+        else:
+            map_ordering = self.map()
+            return torch.ones(1), self.complete_graph_from_ordering(map_ordering), self.regularizer()
+
+    def _training_forward(self):
+        raise NotImplementedError()
+
+    def map(self):
+        """
+        Returns: the most probable ordering (i.e. the mode of the distribution over ordering), that is
+        inv_perm(ascending-argsort(theta).
+
+        """
+        # TODO not super happy of all these reshaping :; maybe we should keep the theta as a vector and reshape it
+        #         when calling sparsemap
+        sorted_indices = torch.argsort(self.theta.view(-1), dim=0)
+        inverse_permutation = torch.argsort(sorted_indices)
+        return inverse_permutation.view(1, -1)
+
+    def complete_graph_from_ordering(self, orderings):
+        """
+
+        Args:
+            orderings: a tensor of orderings of dimensionality [num orderings, d]
+
+        Returns: a tensor of dimensionality [num orderings, d, d] containing
+                    the binary adjacency matrices of the complete dags corresponding to the input ordering
+
+        """
+        return self.M[orderings[..., None], orderings[:, None]]
+
 
 class SparseMapSVStructure(_ScoreVectorStructure):
     """
     Class for learning the score vector with the sparseMAP operator.
     """
 
-    def __init__(self, d, theta_init, l2_reg_strength, smap_init,
-                 smap_iter, smap_tmp=1.) -> None:
+    def __init__(self, d, theta_init, l2_reg_strength=0., smap_init=False,
+                 smap_iter=100, smap_tmp=1.) -> None:
         super().__init__(d, theta_init, l2_reg_strength)
         self.smap_tmp = smap_tmp
         self.smap_init = smap_init
@@ -72,7 +112,7 @@ class SparseMapSVStructure(_ScoreVectorStructure):
             'smap_iter': args.smap_iter,
         }
 
-    def forward(self):
+    def _training_forward(self):
         # call the sparseMAP rank procedure.
         # it returns a vector of probas and a matrix of integer permutations.
         # orderings[0] is the inverse permutation of argsort(self.theta)
@@ -87,7 +127,7 @@ class SparseMapSVStructure(_ScoreVectorStructure):
         # although - not so sure why we should take the MAP,
         # can't we keep the probability distribution also at eval time?
         return (alphas,
-                self.M[orderings[..., None], orderings[:, None]],
+                self.complete_graph_from_ordering(orderings),
                 self.regularizer())
 
 
@@ -105,12 +145,12 @@ class TopKSparseMaxSVStructure(_ScoreVectorStructure):
             'smax_max_k': args.smax_max_k,
         }
 
-    def forward(self):
+    def _training_forward(self):
         # TODO @vlad here need to give a vector as input but sparsemap seem to require d x 1 matrix, right? make same?
         alphas, orderings = sparsemax_rank(self.theta.view(-1) / self.smax_tmp,
                                            max_k=self.smax_max_k)
         return (alphas,
-                self.M[orderings[..., None], orderings[:, None]],
+                self.complete_graph_from_ordering(orderings),
                 self.regularizer())
 
 
---
+++ daguerreo/tests/test_structures.py
@@ -0,0 +1,30 @@
+# from ..structures import SparseMapSVStructure
+from daguerreo import structures as sr
+
+import unittest
+import torch
+
+
+class StructureCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.d = 100
+        init = torch.randn(self.d)
+        self.smap_str = sr.SparseMapSVStructure(self.d, init)
+
+    def test_map(self):
+        for i in range(1):
+            self.setUp()
+            alphas, complete_dags, reg = self.smap_str()
+            map = self.smap_str.map()
+
+            mat_map = self.smap_str.complete_graph_from_ordering(map)
+
+
+
+            test = torch.all(complete_dags[0] == mat_map[0])
+            assert test
+
+
+if __name__ == '__main__':
+    c = StructureCase()
